d0281f124268cb451995127c06713bec0a1c1656
naidinp <56413576+naidinp@users.noreply.github.com>
2020-11-26 08:43:29 +0100

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Bienvenue à tous!"/>
 </template>
 
 <script>
@@ -20,7 +20,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #f03838;
   margin-top: 60px;
 }
 </style>
